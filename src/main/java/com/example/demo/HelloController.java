54769ce8cb45260e3bec14007b8445ab7690313f
Antonio Kobashikawa <akobashikawa@gmail.com>
2025-12-03 15:56:38 -0500

    Agrega import para postmapping

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
